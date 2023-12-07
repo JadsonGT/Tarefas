adcb6c553c891d04e3f0eee7ee942ec5e27bf647
JadsonGT <146020469+JadsonGT@users.noreply.github.com>
2023-12-07 14:23:20 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 
 module.exports = {
-    jwtSecret: 'secreto', // Chave secreta para JWT (substitua por uma chave mais segura na produção)
+    jwtSecret: 'secreto', // Chave secreta para JWT 
   };
   
